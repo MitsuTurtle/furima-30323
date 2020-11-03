f767bc37478dfac513c99a6c3214180582bf4f5b
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-11-03 11:51:19 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,11 @@ class User < ApplicationRecord
  
   # ユーザー本名は、名字と名前がそれぞれ必須であること
   # ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること
-  validates :first_name, :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "には全角（漢字・ひらがな・カタカナ）のみが使えます"}
+  validates :family_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "には全角（漢字・ひらがな・カタカナ）のみが使えます"}
 
   # ユーザー本名のフリガナは、名字と名前でそれぞれ必須であること
   # ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること
-  validates :first_name_kana, :family_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "には全角（カタカナ）のみが使えます"}
+  validates :family_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "には全角（カタカナ）のみが使えます"}
   
   # 生年月日が必須であること
   validates :birthday, presence: true
