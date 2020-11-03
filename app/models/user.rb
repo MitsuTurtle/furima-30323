e9c585c28cf9e0430ed15eee7b9540478cc53690
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-11-03 12:12:58 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,6 @@ class User < ApplicationRecord
   # ニックネームが必須であること
   validates :nickname, presence: true
 
-  # メールアドレスが一意性であること
-
-  # パスワードは、確認用を含めて2回入力すること
-  #  参照：https://railsguides.jp/active_record_validations.html
-  # validates :password, confirmation: true, presence: true
-  # validates :password, presence: true
-
   # パスワード半角英数字混合での入力が必須
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
