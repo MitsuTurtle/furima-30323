53bac5885a253acb856f58b1eb95d6557e073a57
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-11-06 07:24:51 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,10 +7,10 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :postage_payer
   belongs_to :prefecture
-  belongs_to :preparation_day
+  belongs_to :preparation_days
 
-  validates :category, :condition, :prefecture,:Postage_payer, :preparation_day, presence: true
+  validates :category, :condition, :prefecture,:postage_payer, :preparation_days, presence: true
 
-  validates :category_id, :condition_id, :Postage_payer_id, :prefecture_id, :preparation_day_id, numericality: { other_than: 0 }
+  validates :category_id, :condition_id, :postage_payer_id, :prefecture_id, :preparation_days_id, numericality: { other_than: 0 }
 
 end
