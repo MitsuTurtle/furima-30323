0c06c4f797b773a78087930b517bc087332d5d5f
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-11-06 07:42:11 +0900

    ActiveHash発送時期再度作成

--- db/migrate/20201103070404_create_items.rb
+++ db/migrate/20201103070404_create_items.rb
@@ -7,7 +7,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :condition_id, null: false
       t.integer :postage_payer_id, null: false
       t.integer :prefecture_id, null: false
-      t.integer :preparation_days_id, null: false
+      t.integer :shipping_time_id, null: false
       t.integer :price, null: false
       t.references :user, foreign_key: true
       t.timestamps
