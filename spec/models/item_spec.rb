719e418680b9504a75a9c1273e65db01c13653eb
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-11-07 14:48:56 +0900

    itemモデルのテストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe '商品の出品' do
+    context '商品の出品がうまくいくとき' do
+      it 'すべての要素が正しく入力されていると出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品の出品がうまくいかないとき' do
+      it '商品画像がついていないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が未入力だと出品できない' do
+       @item.name =""
+       @item.valid?
+       expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明が未入力だと出品できない' do
+        @item.introduction = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Introduction can't be blank")
+      end
+      it 'カテゴリーが未選択だと出品できない' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 0")
+      end
+      it '商品の状態が未選択だと出品できない' do
+        @item.condition_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 0")
+      end
+      it '配送料の負担が未選択だと出品できない' do
+        @item.postage_payer_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage payer must be other than 0")
+      end
+      it '発送元の地域が未選択だと出品できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 0")
+      end
+      it '発送までの日数が未選択だと出品できない' do
+        @item.shipping_time_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping time must be other than 0")
+      end
+      it '価格が未入力だと出品できない' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格の範囲が、¥300より安いと出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      it '価格の範囲が、¥9,999,999より高いと出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      it '価格が全角数字だと出品できない' do
+        @item.price = "１０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      it '価格に英字が混ざっていると出品できない' do
+        @item.price = "a1000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+    end
+  end
 end
