89a52b1aa791e36c0b292669c1078963996c2edd
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-11-15 15:46:17 +0900

    モデル作成、アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :orders
 
   # ニックネームが必須であること
   validates :nickname, presence: true
